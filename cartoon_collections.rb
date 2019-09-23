4d5d0b076f9401864f1acbf76abd1b2e0f55b928
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-09-23 20:17:04 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index{|x,i| puts "#{(i+1)}. #{x}"  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map!{|x| (x.capitalize!) << "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+#def long_planeteer_calls(array)
+ # array.keep_if{|x| x.length > 4 }
+  #!array.empty?
+#end
+
+def long_planeteer_calls(array)
+  !!array.find_index{|x| x.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+#def find_the_cheese(array)
+ # cheese_types = ["cheddar", "gouda", "camembert"]
+  #array.each_with_index do |x,i|
+   # if cheese_types.include?(array[i])
+    #  return cheese_types.at(cheese_types.find_index(array[i]))
+    #else
+     # return nil
+    #end
+  #end
+#end
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find{|x| cheese_types.include?(x) }
 end
