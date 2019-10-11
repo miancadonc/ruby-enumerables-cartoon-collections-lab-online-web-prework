99245440eae99f9b249e48b359f8298fdc879cc8
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-11 16:10:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,9 @@ def long_planeteer_calls(array)
   !!array.find_index{|x| x.length > 4}
 end
 
-
+def long_planeteer_calls(array)
+  
+end
 
 #def find_the_cheese(array)
  # cheese_types = ["cheddar", "gouda", "camembert"]
